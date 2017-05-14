0ebaa7707f0b3f77b87d036d6d51e7e34bb33eee
mccullen <mccullenlc@gmail.com>
2017-05-14 08:57:59 -0400

    Test out some of typescript type conversion features.

--- McCullen/ClientApp/app/components/projects/projects.ts
+++ McCullen/ClientApp/app/components/projects/projects.ts
@@ -1,4 +1,10 @@
 ﻿import { boardFactory } from "./boardFactory";
+var boardFactory2 = boardFactory as any;
+boardFactory2.test = "hi";
+var jeff = {
+    test: "someValue" as string
+};
+jeff.test = "myprop";
 export class Projects {
-    public prop = boardFactory.test;
+    public prop: any = boardFactory.test;
 }
