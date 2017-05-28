f8181b86b0a5d48e7126696707d33251986f205c
mccullen <mccullenlc@gmail.com>
2017-05-28 17:22:42 -0400

    Get rid of board info idea since aurelia bindings don't work nicely with array indexers. Install jquery and just use that instead.

--- McCullen/ClientApp/app/projects/tic-tac-toe/tic-tac-toe.ts
+++ McCullen/ClientApp/app/projects/tic-tac-toe/tic-tac-toe.ts
@@ -5,12 +5,8 @@ import { DialogService } from "aurelia-dialog";
 import { GameSettings, PlayOption } from "./game-settings";
 import { Router } from "aurelia-router";
 import { KeyValue } from "../../resources/KeyValue";
+import * as $ from "jquery";
 
-class SquareInfo {
-    state: string; // win, lose, tie, played
-    display: string; // depth or piece to display in square
-    depth: number;
-}
 @autoinject()
 export class TicTacToe {
     private board: any;
@@ -23,8 +19,6 @@ export class TicTacToe {
     public showDepth: boolean;
     public gameSettings: any;
     public selectedPlayOption: KeyValue<PlayOption, string>;
-    //boardInfo: any[] = [];
-    boardInfo: SquareInfo[][] = []; // don't forget to initialize here!!!
 
     // Would it be better to inject a board object instead of the factory?
     constructor(
@@ -32,11 +26,6 @@ export class TicTacToe {
             public router: Router) {
         this.dialogService = dialogService;
         this.router = router;
-    }
-    currentPlayerWillWin(state) {
-        let currentPlayer = this.board.getCurrentPlayer();
-        return (currentPlayer === this.board.piece.x && state === this.board.state.xWon) ||
-            (currentPlayer == this.board.piece.o && state === this.board.state.oWon);
     }
     public activate() {
         // Get settings from the user
@@ -52,50 +41,25 @@ export class TicTacToe {
                     this.board = boardFactory({numRows: this.nRows, numColumns: this.nColumns});
                     this.computerPlayer = computerPlayerFactory();
 
-                    // Create empty board with no columns
-                    for (let row = 0; row < this.nRows; ++row) {
-                        this.boardInfo.push(new Array<SquareInfo>());
-                    }
-
-                    this.updateBoardInfo();
                 }
             });
     }
-    updateBoardInfo() {
-                    // Populate the boardInfo array with the values to display
-                    let moveValues = this.computerPlayer.getMoveValues(this.board);
-                    for (let iMove = 0; iMove < moveValues.length; ++iMove) {
-                        let squareInfo: SquareInfo = new SquareInfo();
-                        squareInfo.depth = moveValues[iMove].depth;
-                        if (this.currentPlayerWillWin(moveValues[iMove].state)) {
-                            squareInfo.state = "win";
-                        } else if (moveValues[iMove].state == this.board.state.draw) {
-                            squareInfo.state = "tie";
-                        } else {
-                            squareInfo.state = "lose";
-                        }
-                        squareInfo.display = this.showDepth ? squareInfo.depth.toString() : "";
-                        let row = moveValues[iMove].row;
-                        let column = moveValues[iMove].column;
-                        this.boardInfo[row][column] = squareInfo;
-                    }
-    }
-    public playPiece(row: number, column: number) {
+    playPiece(row: number, column: number) {
         // Disable square of the move just played
         let square = document.getElementById(this.getSquareId(row, column)) as HTMLButtonElement;
         square.disabled = true;
+
+        // Place piece
         square.innerHTML = this.board.getCurrentPlayer();
         this.board.playPiece({row: row, column: column});
-        this.updateBoardInfo();
+    }
+    public onSquareClick(row: number, column: number) {
+        this.playPiece(row, column);
 
         if (this.selectedPlayOption.key === PlayOption.HumanVsComputer) {
-            // It is the computer's turn. Make a move and then disable that square
+            // It is computer player's turn. Make his or her move.
             let bestMove = this.computerPlayer.getBestMove(this.board);
-            let computerSquare = document.getElementById(this.getSquareId(bestMove.row, bestMove.column)) as HTMLButtonElement;
-            computerSquare.innerHTML = this.board.getCurrentPlayer();
-            this.board.playPiece({row: bestMove.row, column: bestMove.column});
-            this.updateBoardInfo();
-            computerSquare.disabled = true;
+            this.playPiece(bestMove.row, bestMove.column);
 
             // Check the state of the board to see if the computer wins
             let state = this.board.getState();
